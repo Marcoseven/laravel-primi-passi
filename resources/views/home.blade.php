1e5cc62a20ed04c72a060b72b5ded509bee01c32
Marcoseven <marco.caliciotti3@gmail.com>
2021-12-16 17:33:27 +0100

    Creato file home.blade.php
    Inseriti due titoli.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <style>
+       /* common_general_rules */
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        /* /common_general_rules */
+
+        h1, h2 {
+            text-align: center;
+            margin: 1rem 0;
+        }
+   </style>
+    <title>Laravel Primi Passi</title>
+</head>
+<body>
+	<h1>Hello World</h1>
+    <h2>First application web with Laravel</h2>
+</body>
+</html>
