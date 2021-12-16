6ae92d841f36fa6fd267a6a101d4cefdbf4a7640
Marcoseven <marco.caliciotti3@gmail.com>
2021-12-16 17:33:45 +0100

    Creato file about.blade.php
    Inseriti due titoli.

---
+++ resources/views/about.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <style>
+       /* common_general_rules */
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        /* /common_general_rules */
+
+        h2 {
+            text-align: center;
+            margin: 1rem 0;
+            font-size: 30px;
+        }
+
+        h3 {
+            text-align: center;
+            font-size: 20px;
+        }
+   </style>
+    <title>About page</title>
+</head>
+<body>
+	<h2>About page</h2>
+    <h3>Mi chiamo Marco Caliciotti. Questa è la prima applicazione creata utilizzando Laravel, e sono entusiasta per questo.</h3>
+</body>
+</html>
